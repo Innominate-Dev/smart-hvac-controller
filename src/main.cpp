ac5e0b0c0b8e7ff2f60232870ac315828c5a59fc
Innominate-Dev <InnominateDev@gmail.com>
2026-05-11 18:47:45 +0100

    Added diagrams and wowki tomi for the hvac system
    - storing humidity and temp aswell as a sensor task

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
-#include <Arduino.h>
+/* 
 
-// put function declarations here:
-int myFunction(int, int);
+DHT22     -> GPIO 16
+POT       -> GPIO 34
+BUTTON 1  -> GPIO 18
+BUTTON 2  -> GPIO 19
+RELAY     -> GPIO 26
+BUZZER    -> GPIO 27
+LED 1     -> GPIO 25
+LED 2     -> GPIO 33
+
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "DHT.h"
+
+#define DHTTYPE DHT22
+#define DHTPIN 16
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+void sensorTask(void *pvParameters) {
+    while(1) {
+        Serial.println("Reading sensor...");
+
+        float temperature, humidity;
+        humidity = dht.readHumidity();
+        temperature = dht.readTemperature();
+
+        Serial.print("Humidity: ");
+        Serial.println(humidity);
+        Serial.print("Temperature: ");
+        Serial.println(temperature);
+        
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    dht.begin();
+    xTaskCreate(sensorTask, "Sensor", 2048, NULL, 2, NULL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // leave empty - FreeRTOS handles everything
 }
